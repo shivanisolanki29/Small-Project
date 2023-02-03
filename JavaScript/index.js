df3f9652c172915219c89a684629c4560f622e42
shivani_simpleDev <shivi.solanki29@gmail.com>
2023-02-03 13:01:40 +0100

    final commit

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,19 @@
-const count = () => {
-  let count = document.getElementById("counter-el").innerText;
-  document.getElementById("counter-el").innerText = +count + 1;
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+// console.log(saveEl);
+let count = 0;
+
+const increment = () => {
+  // let countEl = document.getElementById("count-el").innerText;
+  count += 1;
+  countEl.textContent = count;
+  // console.log(count);
+};
+
+const save = () => {
+  let countstr = " " + count + " - ";
+  saveEl.textContent += countstr;
+  countEl.textContent = 0;
+  count = 0;
+  // console.log(saveEl.innerText);
 };
